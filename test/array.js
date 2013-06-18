21fed00f91a035dc3ef4b93ee18fb063a9ed49c5
pllee <patrick.lorian.lee@gmail.com>
2013-06-17 22:41:17 -0500

    add array insert

--- test/array.js
+++ test/array.js
@@ -7,7 +7,7 @@ describe('Luc Array functions', function() {
 
         Luc.Array.each(arr, function(value, index, a) {
             this.str += value + index + a.length;
-        }, obj)
+        }, obj);
         expect(obj.str).to.eql('a03b13z23');
     });
 
@@ -18,4 +18,32 @@ describe('Luc Array functions', function() {
         expect(Luc.Array.toArray('')).to.eql(['']);
         expect(Luc.Array.toArray([1])).to.eql([1]);
     });
-})
+
+    it('insert', function() {
+        function runAllScenarios(arr1, arr2) {
+            expect(Luc.Array.insert(arr1, arr2, true)).to.be.eql([1,2,3,4,5,6]);
+            expect(Luc.Array.insert(arr2, arr1, true)).to.be.eql([4,5,6,1,2,3]);
+
+            expect(Luc.Array.insert(arr1, arr2, 3)).to.be.eql([1,2,3,4,5,6]);
+            expect(Luc.Array.insert(arr1, arr2, 2)).to.be.eql([1,2,4,5,6,3]);
+            expect(Luc.Array.insert(arr1, arr2, 1)).to.be.eql([1,4,5,6,2,3]);
+            expect(Luc.Array.insert(arr1, arr2, 0)).to.be.eql([4,5,6, 1,2,3]);
+
+            expect(Luc.Array.insert(arr2, arr1, 3)).to.be.eql([4,5,6,1,2,3]);
+            expect(Luc.Array.insert(arr2, arr1, 2)).to.be.eql([4,5,1,2,3,6]);
+            expect(Luc.Array.insert(arr2, arr1, 1)).to.be.eql([4,1,2,3,5,6]);
+            expect(Luc.Array.insert(arr2, arr1, 0)).to.be.eql([1,2,3,4,5,6]);
+
+
+            //test no modify
+            expect(arr1).to.be.eql([1,2,3]);
+            expect(arr2).to.be.eql([4,5,6]);
+        }
+
+        runAllScenarios([1,2,3], [4,5,6]);
+
+        (function(arr1, arr2) {
+            runAllScenarios(arr1, arr2);
+        }([1,2,3], [4,5,6]));
+    });
+});
